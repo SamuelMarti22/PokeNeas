fb2ed908080bd427c6f93bed99121a79b32761b3
Daniela Salazar <dsalazara1@eafit.edu.co>
2026-05-09 12:26:23 -0500

    add home view

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const os = require("os")
 const app = express()
 const port = 80
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 router.get('/', "./src/controllers/PokeneasController.index");
 router.get('/', "./src/controllers/PokeneasController.index");
 
